5faeadb7908c1ad221f8a2a8fa2fdf2f21025a1c
AceCJM <cody@millerfamily.ca>
2023-08-25 15:18:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express();
 
-const PORT = 6969
+const PORT = process.env.PORT
 const greeting = "Server is listening on "
 app.get('/',(req,res)=>{
     res.json({message:"Hello, from backend!"})
